af8e14c81c3bda72342c5e350313786fe7054360
Rebeca <rebecasouk@gmail.com>
2018-08-23 17:43:07 -0700

    Mobile Nav & Hero Img

--- front-page.php
+++ front-page.php
@@ -25,62 +25,76 @@ get_header();
 		endwhile; // End of the loop.
 		?>
 
-			<section>
-			<h1>
-			<?php 
-				if (function_exists('get_field')):
-					if (get_field('headline')):
-						the_field('headline');
-					endif;
-				endif;
-			?>
-			</h1>
-			<?php 
-				if (function_exists('get_field')):
-					if (get_field('tagline')):
-						the_field('tagline');
-					endif;
-				endif;
-			?>
-			<?php 
-				if (function_exists('get_field')):
-					if (get_field('quote_button')):
-						the_field('quote_button');
-					endif;
-				endif;
-			?>
+			<section class="hero">
+				<div class="banner">
+					<img src="<?php bloginfo('template_directory');?>/images/hero-img.png" alt="banner">
+				</div>
+				<article>
+					<h1>
+					<?php 
+						if (function_exists('get_field')):
+							if (get_field('headline')):
+								the_field('headline');
+							endif;
+						endif;
+					?>
+					</h1>
+					<?php 
+						if (function_exists('get_field')):
+							if (get_field('tagline')):
+								the_field('tagline');
+							endif;
+						endif;
+					?>
+					<a class="quote" href="">
+					<?php 
+						if (function_exists('get_field')):
+							if (get_field('quote_button')):
+								the_field('quote_button');
+							endif;
+						endif;
+					?>
+					</a>
+				</article>
 			</section>
 
 			<section class="home-content">
 				<article>
-				<?php 
-				if (function_exists('get_field')):
-					if (get_field('subhead1')):
-						the_field('subhead1');
-					endif;
-				endif;
-				?>
+					<h2>
+						<?php 
+						if (function_exists('get_field')):
+							if (get_field('subhead1')):
+								the_field('subhead1');
+							endif;
+						endif;
+						?>
+					</h2>
+				
 				<div>
-				<?php 
-				if (function_exists('get_field')):
-					if (get_field('content1')):
-						the_field('content1');
+				<p>
+					<?php 
+					if (function_exists('get_field')):
+						if (get_field('content1')):
+							the_field('content1');
+						endif;
 					endif;
-				endif;
-				?>
+					?>
+				</p>
 				</div>
 				<div>
 				</div>
 				</article>
 
 				<article>
-				<?php 
-				if (function_exists('get_field')):
-					if (get_field('subhead2')):
-						the_field('subhead2');
+				<h2>
+					<?php 
+					if (function_exists('get_field')):
+						if (get_field('subhead2')):
+							the_field('subhead2');
+						endif;
 					endif;
-				endif;
-				?>
+					?>
+				</h2>
 				<div>
 				<?php 
 				if (function_exists('get_field')):
